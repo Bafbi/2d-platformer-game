a6adb29fbd919445cbf6baf9b59eb6ae3c5ddbeb
Nathan Eudeline <Bafbi3@gmail.com>
2021-04-21 14:28:08 +0200

    add all class

--- game modules/display.js
+++ game modules/display.js
@@ -3,10 +3,12 @@ export class Display {
         this.buffer = document.createElement("canvas").getContext("2d");
         this.context = canvas.getContext("2d");
     }
+
     renderBackground(color) {
         this.buffer.fillStyle = color;
         this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
     }
+
     render() {
         this.context.drawImage(
             this.buffer.canvas,
@@ -20,6 +22,7 @@ export class Display {
             this.context.canvas.height
         );
     }
+
     resize() {
         this.context.canvas.width = document.documentElement.clientWidth - 32;
         this.context.canvas.height = document.documentElement.clientHeight - 32;
--- main.js
+++ main.js
@@ -4,20 +4,31 @@
 /// Import modules ///
 //////////////////////
 
-import { Display } from "./display.js";
+import { Display } from "./game modules/display.js";
+import { Controller } from "./game modules/controller.js";
+import { Game } from "./game modules/game.js";
+import { Engine } from "./game modules/engine.js";
 
 ///////////////
 /// Objects ///
 ///////////////
 
 const display = new Display(document.getElementById("gameWindow"));
+const controller = new Controller();
+const game = new Game();
+const engine = new Engine(1000 / 30, render(), update());
 
 ////////////////
 /// Function ///
 ////////////////
 
-display.renderBackground("#00f");
-display.render();
+function render() {
+    display.renderBackground("#00f");
+    display.render();
+}
+function update() {
+    game.update;
+}
 
 /////////////////
 /// Listeners ///
